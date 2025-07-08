d73eaf6a9f1668f031e7746da8de6eceb119cca9
Tushar00018 <satalingoltushar@gmail.com>
2025-07-08 22:52:23 +0530

    worked on route and scss setup

---
+++ src/component/Login/index.tsx
@@ -0,0 +1,11 @@
+import type React from "react";
+
+const LoginPage: React.FC = () => {
+    return (
+        <div className="login-page">
+            Login
+        </div>
+    )
+}
+
+export default LoginPage;
---
+++ src/component/Todo/index.tsx
@@ -0,0 +1,11 @@
+import type React from "react";
+
+const Todo: React.FC = () => {
+    return (
+        <div className="todo-container" >
+            Todo
+        </div>
+    )
+}
+
+export default Todo;
